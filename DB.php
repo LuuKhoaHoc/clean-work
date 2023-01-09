5aaaf163915bf6ba5d0e8cbf79df98f151dbeeb1
Luu_Khoa_Hoc <luatluukhoa@gmail.com>
2023-01-09 12:21:01 +0700

    Format code DB.php, config.php

---
+++ DB.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'config.php';
+
+#[AllowDynamicProperties] class DB
+{
+    public static function connect()
+    {
+        $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        if (!$conn) {
+            die('Error: Connection failed ' . mysqli_error());
+        }
+        return $conn;
+    }
+}
+
