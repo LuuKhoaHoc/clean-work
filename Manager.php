8827e7c8b6068b690da42a5069f852b77f75735f
Luu_Khoa_Hoc <luatluukhoa@gmail.com>
2023-01-09 12:19:58 +0700

    add type hitting to func chooseEmp, fetch database, add empty func from AbstractClasses.php to Manager.php

--- Manager.php
+++ Manager.php
@@ -1,1 +1,41 @@
 <?php
+
+use clean_work_design\ManagerAbstractClass;
+
+class Manager extends ManagerAbstractClass {
+    protected function chooseEmployees(int $empNeeded)
+    {
+        $conn = DB::connect();
+        $query = "SELECT * FROM employees ";
+        $result = mysqli_query($conn,$query);
+        if (!$result) {
+            die("Error :" . mysqli_error());
+        } else {
+            $row = mysqli_fetch_all($result);
+            while ($row == $empNeeded) {
+                print_r($row);
+            }
+        }
+        return $row;
+    }
+
+    public function receiveOrder()
+    {
+        // TODO: Implement receiveOrder() method.
+    }
+
+    public function callCustomer()
+    {
+        // TODO: Implement callCustomer() method.
+    }
+
+    public function receiveCleaningTeam()
+    {
+        // TODO: Implement receiveCleaningTeam() method.
+    }
+
+    public function receivePayment()
+    {
+        // TODO: Implement receivePayment() method.
+    }
+}
