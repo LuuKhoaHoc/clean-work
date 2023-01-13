aaa858212c5f1ad176ac019c4403fa9b198abb03
Luu_Khoa_Hoc <luatluukhoa@gmail.com>
2023-01-13 14:32:36 +0700

    Complete sprint 1 [develop-feature-receiveOrder]

--- receiveOrder.php
+++ receiveOrder.php
@@ -1,8 +1,12 @@
-<pre>
 <?php
-
+include_once "Manager.php";
+$manager = new Manager();
 var_dump($_POST);
+$name = $_POST["customer-name"];
+$email = $_POST["customer-email"];
+$phone = $_POST["customer-phone"];
+$address = $_POST["customer-address"];
+$sti = (int)$_POST["service-type-id"];
+$cmt = $_POST["comment"];
 
-?>
-</pre>
-ahihi
+$manager->receiveOrder($name, $email, $phone, $address, $sti, $cmt);
