753dd82882fdd5d2e3562d454f745380b6a0856b
Luu_Khoa_Hoc <luatluukhoa@gmail.com>
2023-02-06 11:38:47 +0700

    thêm testInput vào receiveOrder.php tăng bảo mật,

--- receiveOrder.php
+++ receiveOrder.php
@@ -2,11 +2,19 @@
 include_once "Manager.php";
 $manager = new Manager();
 var_dump($_POST);
-$name = $_POST["customer-name"];
-$email = $_POST["customer-email"];
-$phone = $_POST["customer-phone"];
-$address = $_POST["customer-address"];
-$sti = (int)$_POST["service-type-id"];
-$cmt = $_POST["comment"];
+$name = testInput($_POST["customer-name"]);
+$email = testInput($_POST["customer-email"]);
+$phone = testInput($_POST["customer-phone"]);
+$address = testInput($_POST["customer-address"]);
+$sti = testInput((int)$_POST["service-type-id"]);
+$cmt = testInput($_POST["comment"]);
 
 $manager->receiveOrder($name, $email, $phone, $address, $sti, $cmt);
+
+function testInput($data): string
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
