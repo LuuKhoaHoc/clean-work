24a3fe57089e92888bd4ee2f2bd3fa3d329395e7
MeoAhihi <viphongly2804@gmail.com>
2023-03-26 22:26:25 +0700

    test

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 
 include 'site/controller/Guest_Display_Content_Controller.php';
 $controler = new Guest_Display_Content_Controller();
-$controler->showServiceDetailAction();
+$controler->showLoginAction();
 
 // include 'site/controller/Customer_Display_Content_Controller.php';
 // $controler = new Customer_Display_Content_Controller();
