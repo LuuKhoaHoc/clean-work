190a3e1c5994a9873a35a2ac9cc8898433f9c70c
MeoAhihi <viphongly2804@gmail.com>
2023-03-26 22:33:35 +0700

    test 4

--- test.php
+++ test.php
@@ -1,9 +1,11 @@
 <?php
 
-include 'site/controller/Guest_Display_Content_Controller.php';
-$controler = new Guest_Display_Content_Controller();
-$controler->showServiceDetailAction();
-
+// include 'site/controller/Guest_Display_Content_Controller.php';
+// $controler = new Guest_Display_Content_Controller();
+// $controler->showServiceDetailAction();
+include('site/view/Guest_View.php');
+$view = new Guest_View();
+$view->ServiceDetailView($service);
 // include 'site/controller/Customer_Display_Content_Controller.php';
 // $controler = new Customer_Display_Content_Controller();
 // $controler->showProfileAction('');
