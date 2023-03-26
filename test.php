2ad90234080bf0367c45f27243e20b248ed721e6
MeoAhihi <viphongly2804@gmail.com>
2023-03-26 22:25:07 +0700

    test detail

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+
+include 'site/controller/Guest_Display_Content_Controller.php';
+$controler = new Guest_Display_Content_Controller();
+$controler->showServiceDetailAction();
+
+// include 'site/controller/Customer_Display_Content_Controller.php';
+// $controler = new Customer_Display_Content_Controller();
+// $controler->showProfileAction('');
